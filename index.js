9ae20c6a52b1e49b0ed0196fe56b441d7e2c3033
Andrew Waller <andrewhwaller@gmail.com>
2019-09-30 20:55:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ let main = document.querySelector('main')
 main.parentNode.removeChild(main);
 
 const newHeader = document.createElement('h1')
-let newHeader.id = "victory"
+newHeader.id = "victory"
 
 newHeader.innerHTML = "YOUR-NAME is the champion"
